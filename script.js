9aa616c15f968dc2519754523d8e6cecc736427b
Nastya Semyonova <nastya.semyonova.ss@gmail.com>
2022-02-18 15:47:48 +0600

    add delete function

--- script.js
+++ script.js
@@ -26,11 +26,18 @@ UI.prototype.addBookToList = function (book) {
   const thTitile = document.createElement("th");
   const thAuthor = document.createElement("th");
   const thISB = document.createElement("th");
+  const deletElem = document.createElement("th");
+  deletElem.className = "delet";
+  const delet = document.createElement("img");
+  delet.src = "./x.svg";
+  deletElem.style.cursor = "pointer";
 
   output.appendChild(tr);
   tr.appendChild(thTitile);
   tr.appendChild(thAuthor);
   tr.appendChild(thISB);
+  tr.appendChild(deletElem);
+  deletElem.appendChild(delet);
   thISB.innerText = this.isb;
   thAuthor.innerText = this.author;
   thTitile.innerText = this.title;
@@ -38,33 +45,56 @@ UI.prototype.addBookToList = function (book) {
 // Clear fields
 UI.prototype.clearFields = function () {
   output.addEventListener("click", (e) => {
-    if (e.target.closest(".tr")) {
+    if (e.target.closest(".delet")) {
       e.target.closest(".tr").remove();
     }
   });
 };
+UI.prototype.showAlert = function (msg, className) {
+  const div = document.createElement("div");
+  div.className = `alert ${className}`;
+
+  div.appendChild(document.createTextNode(msg));
+
+  const container = document.querySelector(".container");
+  const title = document.querySelector(".title");
+
+  container.insertBefore(div, title);
+
+  setTimeout(function () {
+    document.querySelector(".alert").remove();
+  }, 3000);
+};
 
 btn.addEventListener("click", (e) => {
+  // if (
+  //   inputAuthor.value === "" ||
+  //   inputISB.value === "" ||
+  //   inputTitle.value === ""
+  // ) {
+  //   let error = document.createElement("div");
+  //   error.className = "border p-3";
+
+  //   error.style.width = "100%";
+  //   error.style.color = "#B33030";
+  //   error.style.backgroundColor = "#FFB5B5";
+
+  //   error.innerText = "Заполните все поля!";
+  //   container.appendChild(error);
+  //   table.style.display = "none";
+  // }
+  const result = new UI();
   if (
     inputAuthor.value === "" ||
     inputISB.value === "" ||
     inputTitle.value === ""
   ) {
-    let error = document.createElement("div");
-    error.className = "border p-3";
-
-    error.style.width = "100%";
-    error.style.color = "#B33030";
-    error.style.backgroundColor = "#FFB5B5";
-
-    error.innerText = "Заполните все поля!";
-    container.appendChild(error);
-    table.style.display = "none";
+    result.showAlert("Please fill in all fields!", "error");
+  } else {
+    result.addBookToList();
+    result.showAlert("Book added!", "success");
+    result.clearFields();
   }
-  const result = new UI();
-
-  result.addBookToList();
-  result.clearFields();
 
   e.preventDefault();
 });
